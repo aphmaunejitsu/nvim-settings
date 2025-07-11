7d0068a4c3cbe390926114a331a1810891a024fe
albumensan <ochi.i@bankers.co.jp>
2025-07-11 16:01:26 +0900

    Remove legacy configuration files and environment detection for Neovim setup, consolidating plugin management and key mappings for a cleaner architecture.

--- lua/base.lua
+++
@@ -1,9 +0,0 @@
--- 基本設定
-vim.cmd("autocmd!")
-
--- エンコーディング設定
-vim.scriptencoding = "utf-8"
-
--- netrwを無効化（nvim-treeを使用するため）
-vim.g.loaded_netrw = 1
-vim.g.loaded_netrwPlugin = 1
--- lua/config/basic-plugins-vscode.lua
+++
@@ -1,7 +0,0 @@
--- VSCode環境用の基本プラグイン設定
-
--- nvim-autopairs (括弧の自動補完)
-require('nvim-autopairs').setup{}
-
--- Comment.nvim (コメントトグル)
-require('Comment').setup()
